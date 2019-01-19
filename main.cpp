adbed4f502ffa4923a751a8fb8e3aa82d3bf73de
aicheelyelkory <46850583+aicheelyelkory@users.noreply.github.com>
2019-01-19 22:13:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "CalculusOfPmt.h"
-#include "CalculusOfPv.h"
-#include "CalculusOfPeriods.h"
+#include "CalculOfPmt.h"
+#include "CalculOfPv.h"
+#include "CalculOfPeriods.h"
 int main() {
   //  std::cout << "Hello, World!" << std::endl;
   //  return 0;
