974b3076e571830521cbf8fe6eca1ff094108d9c
aicheelyelkory <46850583+aicheelyelkory@users.noreply.github.com>
2019-01-19 21:53:38 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "CalculusOfPmt.h"
+#include "CalculusOfPv.h"
+#include "CalculusOfPeriods.h"
+int main() {
+  //  std::cout << "Hello, World!" << std::endl;
+  //  return 0;
+   //CalculOfPMT();
+    //CalculOfPV();
+    CalculOfPeriods();
+
+}
