9a0b74304cf32ce8c4dbf0b4073716915da7aca2
aicheelyelkory <46850583+aicheelyelkory@users.noreply.github.com>
2019-01-20 11:28:22 +0100

    Update YieldToMaturity.h

--- YieldToMaturity.h
+++ YieldToMaturity.h
@@ -8,50 +8,40 @@ using namespace std;
 
 double f(double PMT, double PV, int n, double r) {
     double temp;
-    temp = PMT/(r*(1+(1/pow(1+r, n)-1))) - PV;
+    temp = PV - PMT/(r*(1+(1/pow(1+r, n)-1)));
     return temp;
 }
 
 
-void YieldToMaturity(){
-double PMT, PV, n;
-double r0, r1, r2, eps;
-cout
-<< " We want to find an approximate r related to the formula that we have and an error of approximation"
-<< "\n";
-cout << "Provide epsilon, the error" << endl;
-cin >> eps;
-while (eps <= 0) {
-cout << "Provide a striclty positive" << endl;
-cin >> eps;
-}
-cout << "Provide PMT the installment" << endl;
-cin >> PMT;
-cout << "Provide PV the present value" << endl;
-cin >> PV;
-cout << "Provide the number of period n" << endl;
-cin >> n;
-while (n < 0) {
-cout << "Provide a non negative number" << endl;
-cin >> n;
-}
-cout << "Provide an initial r0 and r1 such that both have to be between 0 and 1 and the sum equal or below to 1"
-<< "\n";
-cout << "Provide r0" << endl;
-cin >> r0;
-cout << "Provide r1" << endl;
-cin >> r1;
-while ((r1 + r0 > 1) or (r1 > 1) or (r0 > 1) or (r1 <= 0) or (r0 <= 0)) {
-cout << "Provide a feasible couple (r0,r1) " << "\n";
-cout << "Provide r0" << endl;
-cin >> r0;
-cout << "Provide r1" << endl;
-cin >> r1;
-}
-while (abs(r0 - r1) > eps) {
-r2 = r1 - (f(r0,PMT,PV,n)*(r1 - r0)/(f(r0,PMT,PV,n)-f(r1,PMT,PV,n)));
-r0 = r1;
-r1 = r2;
-}
-cout << "r is equal to " << r2 << " with respect to this approximation error : " << eps << endl;
+double YieldToMaturity(){
+
+    double PMT,PV,r, k,rNext, temp;
+    int n;
+
+    //PROVIDING VALUES
+    //double PV, PMT;
+    //int n;
+
+    cout << "Please provide:" << endl;
+    cout << "The Present Value:"; cin >> PV;
+    cout << "PMT:"; cin >> PMT;
+    cout << "The number of periods"; cin >> n ;
+    //PV = PresentValue;
+
+    cout << "PMT: " << PMT << endl;
+    cout << "n: " << n << endl;
+
+    // starting points
+    r = approximateYTM(PMT,PV,n);
+    rNext = 0.95*r;
+
+    while (abs(f(PMT,PV,n,rNext))> 0.001){
+        temp = rNext;
+        rNext = rNext - (f(PMT,PV,n,rNext)*(rNext - r))/(f(PMT,PV,n,rNext)-f(PMT,PV,n,r));
+        r = temp;
+
+    }
+
+    cout << "objective function: " << f(PMT,PV,n,rNext) << endl;
+    cout << "YTM : " << (rNext * k)*100 << "% ." << endl;
 }
