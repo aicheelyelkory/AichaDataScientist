9a68a04708b184beb8573190492add28a652ffcb
aicheelyelkory <46850583+aicheelyelkory@users.noreply.github.com>
2019-01-20 09:53:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include "CalculOfPMT.h"
 #include "CalculOfPV.h"
 #include "CalculOfPeriods.h"
+#include "YieldToMaturity.h"
 int main() {
   //  std::cout << "Hello, World!" << std::endl;
   //  return 0;
    //CalculOfPMT();
     //CalculOfPV();
     CalculOfPeriods();
+  //YieldToMaturity();
 
 }
